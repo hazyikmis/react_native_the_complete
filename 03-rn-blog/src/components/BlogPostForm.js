9d406704e1e92f51be4da099f1ad3a6783ae70a3
hazyikmis <hazyikmis@yahoo.com>
2020-10-08 15:42:44 +0200

    03-BlogPostForm generic form used form CreateScreen

---
+++ 03-rn-blog/src/components/BlogPostForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const BlogPostForm = ({ onSubmit }) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState(``);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.labelStyle}>Enter Title:</Text>
+      <TextInput
+        style={styles.inputStyle}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.labelStyle}>Enter Content</Text>
+      <TextInput
+        style={styles.inputStyle}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
+      <Button title="Save Blog Post" onPress={() => onSubmit(title, content)} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  inputStyle: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  labelStyle: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+});
+
+export default BlogPostForm;
--- 03-rn-blog/src/screens/CreateScreen.js
+++ 03-rn-blog/src/screens/CreateScreen.js
@@ -1,61 +1,20 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import BlogPostForm from '../components/BlogPostForm';
 import { Context as BlogContext } from '../context/BlogContext';
 
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState(``);
-
   const { addBlogPost } = useContext(BlogContext);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.labelStyle}>Enter Title:</Text>
-      <TextInput
-        style={styles.inputStyle}
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-      />
-      <Text style={styles.labelStyle}>Enter Content</Text>
-      <TextInput
-        style={styles.inputStyle}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-      />
-      <Button
-        title="Add Blog Post"
-        onPress={() => {
-          //this scenario below has some flaws, because adding a post is a async in nature
-          //and there might be an error
-          /*
-              addBlogPost(title, content);
-              navigation.navigate('ScreenIndex');
-              */
-          //we need to change addBlogPost to accept a callback function
-          addBlogPost(title, content, () => {
-            navigation.navigate('ScreenIndex');
-          });
-        }}
-      />
-    </View>
+    <BlogPostForm
+      onSubmit={(title, content) => {
+        addBlogPost(title, content, () => navigation.navigate('ScreenIndex'));
+      }}
+    />
   );
 };
 
-const styles = StyleSheet.create({
-  container: {},
-  inputStyle: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 15,
-    padding: 5,
-    margin: 5,
-  },
-  labelStyle: {
-    fontSize: 20,
-    marginBottom: 5,
-    marginLeft: 5,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default CreateScreen;
