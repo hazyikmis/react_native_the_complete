47c4a3cc2e4208622c5dcd1125ff0c2e6a51d2e3
hazyikmis <hazyikmis@yahoo.com>
2020-10-11 16:10:47 +0200

    04-express app installed, mongodb connection is ok

---
+++ 04-rn-tracking/track-server/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+require('dotenv').config();
+
+let mongoUri = process.env.MONGO_DB_CONN_STR;
+mongoUri = mongoUri
+  .replace('<username>', process.env.MONGO_DB_USER)
+  .replace('<password>', process.env.MONGO_DB_PWD)
+  .replace('<dbname>', process.env.MONGO_DB_NAME);
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connection is successful...');
+});
+mongoose.connection.on('error', (err) => {
+  console.log('ERROR connecting to MongoDB!', err);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000...');
+});
